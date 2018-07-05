4d952d85145a8d9570fde2b067036620466d3f63
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-05 09:25:59 +0100

    write database schema

---
+++ backend/seed/build.sql
@@ -0,0 +1,64 @@
+DROP DATABASE IF EXISTS organise;
+CREATE DATABASE organise;
+\c organise;
+
+CREATE TABLE activist (
+    id SERIAL PRIMARY KEY,
+    email TEXT,
+    name TEXT,
+    last_login TIMESTAMP,
+    joined DATE DEFAULT current_date
+);
+
+CREATE TABLE campaign (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    logo TEXT
+);
+
+CREATE TABLE campaign_admin (
+    id SERIAL PRIMARY KEY,
+    activist_id INT REFERENCES activist(id),
+    campaign_id INT REFERENCES campaign(id)
+);
+
+CREATE TABLE campaign_member (
+    id SERIAL PRIMARY KEY,
+    activist_id INT REFERENCES activist(id),
+    campaign_id INT REFERENCES campaign(id)
+);
+
+CREATE TABLE task (
+    id SERIAL PRIMARY KEY,
+    campaign_id INT REFERENCES campaign(id),
+    instructions TEXT,
+    due_date DATE
+);
+
+CREATE TABLE task_status (
+    id SERIAL PRIMARY KEY,
+    task_id INT REFERENCES task(id),
+    activist_id INT REFERENCES activist(id),
+    completed BOOLEAN
+);
+
+CREATE TABLE vote (
+    id SERIAL PRIMARY KEY,
+    question TEXT,
+    closes DATE,
+    campaign_id INT REFERENCES campaign(id)
+);
+
+CREATE TABLE vote_choice (
+    id SERIAL PRIMARY KEY,
+    vote_id INT REFERENCES vote(id),
+    text TEXT
+);
+
+CREATE TABLE vote_cast (
+    id SERIAL PRIMARY KEY,
+    vote_id INT REFERENCES vote(id),
+    vote_choice_id INT REFERENCES vote_choice(id),
+    activist_id INT REFERENCES activist(id)
+);
