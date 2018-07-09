c3aaa4e08d9f3719a703a1c2cd66aae7b6c230e8
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-09 23:26:32 +0100

    tweak db.js to use non-committed secrets.js, will be replaced with environment variables later

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,15 +1,14 @@
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
-const {ORGANISE_DB_USER, ORGANISE_DB_PW} = process.env;
+const {user, password} = require('./secrets');
 
 const connection = {
   host: 'localhost',
   port: 5432,
   database: `organise_${process.env.NODE_ENV}`,
-  user: ORGANISE_DB_USER,
-  password: ORGANISE_DB_PW
+  user,
+  password
 };
-
 
 const pgp = require('pg-promise')();
 const db = pgp(connection);
