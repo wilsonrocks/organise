0c4620de0712f5f50524ee8954135ab308e3244d
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-12 15:23:15 +0100

    apply auth middleware to all routes starting with /api/v1 except /api/v1 which will return help

--- backend/routes/v1/activist.route.js
+++ backend/routes/v1/activist.route.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const {authenticate} = require('../../app');
+
 const {getDetails} = require('../../controllers/activist.controller');
 
 router.route('/:id')
--- backend/routes/v1/api.route.js
+++ backend/routes/v1/api.route.js
@@ -1,6 +1,10 @@
-const router = require('express').Router();
+const {authenticate} = require('../../auth');
 const activistRouter = require('./activist.route');
 
+const router = require('express').Router();
+
+router.all(/\/.+/, authenticate);
+router.get('', (req, res)=>res.send('\nThere will be some API help here, in time\n'));
 router.use('/activist', activistRouter);
 
 module.exports = router;
