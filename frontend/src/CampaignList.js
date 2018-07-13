0c1f966b903cb6779a41af8562630018a9604765
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-13 18:58:01 +0100

    split campaigns into admin and members

--- frontend/src/CampaignList.js
+++ frontend/src/CampaignList.js
@@ -3,15 +3,29 @@ import {Link} from 'react-router-dom';
 
 
 function CampaignList ({campaigns}) {
+
+  const adminCampaigns = campaigns.filter(campaign => campaign.membership === 'admin');
+  const memberCampaigns = campaigns.filter(campaign => campaign.membership === 'member');
+
   return (
     <nav>
       <ul>
       <h2>Campaigns</h2>
-      {campaigns.map(
+      <h3>Member</h3>
+
+      {memberCampaigns.map(
         ({id, name}) => (
-          <li><Link key={id} to="#">{name}</Link></li>
+          <li key={id}><Link to="#">{name}</Link></li>
         )
       )}
+
+      <h3>Admin</h3>
+      {adminCampaigns.map(
+        ({id, name}) => (
+          <li key={id}><Link to="#">{name}</Link></li>
+        )
+      )}
+
       </ul>
     </nav>
   );
