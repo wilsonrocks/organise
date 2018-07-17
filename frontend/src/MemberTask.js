06d065d08c63a7ea9cd93d479df3df0630f25f9d
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-17 13:32:51 +0100

    use momentJS to display due dates nicely

--- frontend/src/MemberTask.js
+++ frontend/src/MemberTask.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import moment from 'moment';
 
 function MemberTask ({instructions, dueDate, doneCallback}) {
   return (
   <div style={{border: 'solid 1px'}}>
     <p>{instructions}</p>
-    <p>Due {dueDate} </p>
+    <p>Due {moment(due_date).fromNow()} </p>
     <button onClick={doneCallback}>Done!</button>
   </div>
   );
