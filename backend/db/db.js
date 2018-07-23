d9efa912823e6eb36334f5b6c6e74b37d1706775
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-23 21:52:57 +0100

    use dotenv to set up environment

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,12 @@
+require('dotenv').config();
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
-
-const {user, password} = require('./secrets');
 
 const connection = {
   host: 'localhost',
   port: 5432,
   database: `organise_${process.env.NODE_ENV}`,
-  user,
-  password
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 };
 
 const pgp = require('pg-promise')();
