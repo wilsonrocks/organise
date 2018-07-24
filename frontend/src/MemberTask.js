577872dba6c2b0228523798f21836e92bf32ad21
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-24 09:23:06 +0100

    style MemberTask using Bulma

--- frontend/src/MemberTask.js
+++ frontend/src/MemberTask.js
@@ -3,10 +3,17 @@ import moment from 'moment';
 
 function MemberTask ({instructions, due_date, doneCallback}) {
   return (
-  <div>
-    <p>{instructions}</p>
-    <p>Due {moment(due_date).fromNow()} </p>
-    <button onClick={doneCallback}>Done!</button>
+  <div className="media task">
+    
+    <div className="media-left">
+      <button className="button" onClick={doneCallback}>Done!</button>
+    </div>
+    
+    <div className="media-content">
+      <p>{instructions}</p>
+      <p className="has-text-right">Due {moment(due_date).fromNow()} </p>
+    </div>
+
   </div>
   );
 }
